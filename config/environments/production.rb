2e0d4a45e2ce617640c2d72598a26a13e9f446f8
Andreas Muehling <andreas.muehling@tum.de>
2015-08-20 14:10:00 +0200

    Deploy?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.otf)
+  config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
   # Generate digests for assets URLs.
   config.assets.digest = true
