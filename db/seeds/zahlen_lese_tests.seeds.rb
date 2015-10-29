f1f68ca40c732f34a5b5c7bf0a919b2ca8896a66
Andreas Muehling <andreas.muehling@tum.de>
2015-10-29 14:03:34 +0100

    Zahlen lesen

---
+++ db/seeds/zahlen_lese_tests.seeds.rb
@@ -0,0 +1,37 @@
+# -*- encoding : utf-8 -*-
+
+tN1 = TestSpeed.create(name: "Zahlen lesen - N1", len: 11, info: "", short_info: "", time: 60)
+
+(0..10).to_a.each do |i|
+  it = tN1.items.build(itemtext: i.to_s, difficulty: 0)
+  it.save
+end
+
+tN1.save
+
+tN2 = TestSpeed.create(name: "Zahlen lesen - N2", len: 21, info: "", short_info: "", time: 60)
+
+(0..20).to_a.each do |i|
+  it = tN2.items.build(itemtext: i.to_s, difficulty: 0)
+  it.save
+end
+
+tN2.save
+
+tN3 = TestSpeed.create(name: "Zahlen lesen - N3", len: 80, info: "", short_info: "", time: 60)
+
+(21..100).to_a.each do |i|
+  it = tN3.items.build(itemtext: i.to_s, difficulty: 0)
+  it.save
+end
+
+tN3.save
+
+tN4 = TestSpeed.create(name: "Zahlen lesen - N4", len: 100, info: "", short_info: "", time: 60)
+
+(101..1000).to_a.each do |i|
+  it = tN4.items.build(itemtext: i.to_s, difficulty: 0)
+  it.save
+end
+
+tN4.save
