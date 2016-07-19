963341c052f0cff5f14850e0c3c0ecd40df2b3d1
Andreas Muehling <andreas.muehling@tum.de>
2016-07-19 14:38:52 +0200

    Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_controller.relative_url_root = '/levumi'
+  #config.action_controller.relative_url_root = ''
 
   # Code is not reloaded between requests.
   config.cache_classes = true
