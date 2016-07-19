e4d2d2fb5ad58b7d118331251c83dfef694a5292
Andreas Muehling <andreas.muehling@tum.de>
2016-07-19 14:23:00 +0200

    Cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 set :stage, :production
-set :branch, 'master'
+set :branch, 'deploy'
 
 server "levumi.informatik.uni-kiel.de", roles: %w{web app db}, user: 'levumi', password: 'levumi', :primary => true
 
