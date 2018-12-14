87cbcfc5097ad809b2c6de75a82743cde83a7242
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2018-12-14 16:05:39 +0100

    Archivieren fertig

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,19 +5,28 @@ class GroupsController < ApplicationController
     @groups = @user.groups.all
   end
 
+  #POST /groups.html
+  def create
+    @user.groups.create(params.require(:group).permit(:label))
+    redirect_to :students
+  end
+
   #PUT /groups/:id
   def update
     g = @user.groups.find(params[:id])
     unless g.nil?
       g.update_attributes(params.require(:group).permit(:label, :archive))
     end
-    @groups = @user.groups.all
+    render status: :ok
   end
 
-  #POST /groups.html
-  def create
-    @user.groups.create(params.require(:group).permit(:label))
-    redirect_to :students
+  #DEL /groups/:id
+  def destroy
+    g = @user.groups.find(params[:id])
+    unless g.nil?
+      g.destroy
+    end
+    render status: :ok
   end
 
   private
--- app/javascript/packs/backend/group-tab.vue
+++ app/javascript/packs/backend/group-tab.vue
@@ -1,28 +1,70 @@
 <template>
 
-     <div v-if="group.archive == false">
+     <div v-if="groups[index].archive == false">
          <b-table hover :items="students" :fields="['name', 'login']"></b-table>
          <i class="fas fa-user-plus"></i> Schüler_in hinzufügen
          <i class="fas fa-edit"></i> Klasse bearbeiten
-         <a :href="'/groups/' + group.id" data-method="put" data-remote="true" data-params="group[archive]=1"><i class="fas fa-file-export"></i> Klasse in Archiv verschieben</a>
-         <i class="fas fa-trash"></i> Klasse löschen
+         <a
+                 :href="'/groups/' + groups[index].id"
+                 data-method="put"
+                 data-remote="true"
+                 data-params="group[archive]=1"
+                 v-on:ajax:success="set_archive(index, true)"
+         >
+             <i class="fas fa-file-export"></i> Klasse in Archiv verschieben
+         </a>
      </div>
 
     <div v-else>
         <p>Schüler_innen: {{students.length}}</p>
-        <a :href="'/groups/' + group.id" data-method="put" data-remote="true" data-params="group[archive]=0"><i class="fas fa-file-import"></i> Klasse aus dem Archiv holen</a>
+        <button class="btn btn-primary"
+                :href="'/groups/' + groups[index].id"
+                data-method="put"
+                data-remote="true"
+                data-params="group[archive]=0"
+                v-on:ajax:success="set_archive(index, false)"
+        >
+            <i class="fas fa-file-import"></i> Klasse aus dem Archiv holen
+        </button>
+        <br/>
+        <button class="btn btn-danger"
+                :href="'/groups/' + groups[index].id"
+                data-method="delete"
+                data-remote="true"
+                data-confirm="Die Klasse wird mit allen Schüler_innen und allen Messergebnissen gelöscht! Sind Sie sicher?"
+                v-on:ajax:success="remove(index)"
+        >
+            <i class="fas fa-trash"></i> Klasse löschen
+        </button>
     </div>
 
 </template>
 
 <script>
+    import Rails from 'rails-ujs'
+
     export default {
         props: {
-            group: Object,
+            groups: Array,
+            index: Number,
             students: Array
+        },
+        methods: {
+            update() {
+                this.$emit("update:groups", this.groups);
+            },
+            set_archive(i, a) {
+                this.groups[i].archive = a;
+                this.update;
+            },
+            remove(i) {
+                this.groups.splice(i, 1);
+                this.update;
+            }
         },
         name: "group-tab"
     }
+
 </script>
 
 <style>
