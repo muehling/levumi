357d90c203b1152e26b2152426d3865c64e35b39
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-05-16 21:00:28 +0200

    Extra Graphview Component

---
+++ app/javascript/packs/home/graph-view.vue
@@ -0,0 +1,190 @@
+<template>
+    <div>
+        <b-row>
+            <b-button-group class='ml-3'>
+                <b-button class='mr-2'
+                          size='sm'
+                          variant='outline-primary'
+                          :pressed="student_selected == -1"
+                          @click="student_selected = -1; view_selected = 0;">Ganze Klasse</b-button>
+                <b-button v-for="(student, index) in students"
+                          :key="student.id"
+                          class='mr-2'
+                          size='sm'
+                          variant='outline-primary'
+                          :pressed="student_selected == index"
+                          @click="student_selected = index"
+                >
+                    {{student.name}}
+                </b-button>
+            </b-button-group>
+        </b-row>
+        <b-row class='mt-2'>
+            <b-button-group class='ml-3'>
+                <b-button
+                        v-for="(view, index) in configuration.views"
+                        v-if="(student_selected == -1 && view.group) || (student_selected > -1 && view.student)"
+                        :key="index"
+                        class='mr-2'
+                        size='sm'
+                        variant='outline-secondary'
+                        :pressed="view_selected == index"
+                        @click="view_selected = index"
+                >
+                    {{ view.label }}
+                </b-button>
+            </b-button-group>
+        </b-row>
+        <b-row>
+            <div id='chart_area'>
+                <apexchart ref='chart' width='100%' height='500px' :options="options" :series="series"></apexchart>
+            </div>
+        </b-row>
+        <b-row>
+            <div class='ml-3'>
+                <b-button @click="add_annotation">Kommentar einfügen</b-button>
+            </div>
+        </b-row>
+    </div>
+</template>
+
+<script>
+    import cloner from 'cloner';
+    export default {
+        props: {
+            students: Array,
+            results: Array,
+            configuration: Object,
+        },
+        data: function () {
+            return {
+                view_selected: 0,
+                student_selected: -1,
+                default_options: {
+                    chart: {
+                        id: 'chart',
+                        toolbar: {
+                            show: false,
+                        },
+                    },
+                    grid: {
+                        padding: {
+                            right: 15,
+                            left: 15
+                        }
+                    },
+                    xaxis: {
+                        type: 'category',
+                        categories: []
+                    },
+                    markers: {
+                        size: 4,
+                        hover: {
+                            sizeOffset: 2
+                        }
+                    },
+                    stroke: {
+                        width: 1
+                    },
+                    tooltip: {
+                        shared: true
+                    }
+                }
+            }
+        },
+        computed: {
+            options: function () { //Options für die gewählte Ansicht mit den globalen Options vereinen
+                let weeks = [];
+                for (let i = 0; i < this.results.length; ++i)
+                    if (this.results[i].test_week != null)
+                        weeks.push(this.results[i].test_week);
+                weeks = weeks.filter((v, i, a) => a.indexOf(v) === i);
+                console.log(JSON.parse(JSON.stringify(this.default_options)));
+                let opt = JSON.parse(JSON.stringify(this.default_options));
+                console.log(opt);
+                opt.xaxis.categories = cloner.shallow.copy(weeks);
+                console.log(opt);
+                opt = cloner.deep.merge(opt, JSON.parse(JSON.stringify(this.configuration.views[this.view_selected].options)));
+                console.log(opt);
+                return opt;
+            },
+            series: function () {  //Bereitet die Results-Daten so auf, dass sie im Chart dargestellt werden können.
+                let res = [];
+                const view = this.configuration.views[this.view_selected];
+                //Ein "leeres" Objekt für alle Datenserien anlegen
+                if (this.student_selected == -1) {
+                    for (let s = 0; s < this.students.length; ++s)
+                        res.push({
+                            'name': this.$parent.student_name(this.students[s].id),
+                            'data': []
+                        });
+                } else {
+                    if (view.series == undefined) {
+                        res.push({
+                            'name': this.$parent.student_name(this.students[this.student_selected].id),
+                            'data': []
+                        });
+                    } else
+                        for (let s = 0; s < view.series.length; ++s)
+                            res.push({
+                                'name': view.series[s],
+                                'data': []
+                            });
+                }
+                //Leere Objekte füllen
+                for (let i = 0; i < this.results.length; ++i) {
+                    if (this.student_selected == -1 || view.series == undefined) {
+                        let student = this.$parent.student_name(this.results[i].student_id);
+                        let week = this.results[i].test_week;
+                        for (let r = 0; r < res.length; ++r) {
+                            if (res[r].name == student) {
+                                res[r].data.push({
+                                    x: this.results[i].test_week,
+                                    y: this.results[i].results[view.label].toFixed(2), //Macht das Runden hier immer Sinn?
+                                });
+                                break;
+                            }
+                        }
+                    } else if (this.results[i].student_id == this.students[this.student_selected].id) {
+                        for (let r = 0; r < view.series.length; ++r) {
+                            res[r].data.push({
+                                x: this.results[i].test_week,
+                                y: this.results[i].results[view.label][view.series[r]].toFixed(2), //Macht das Runden hier immer Sinn?
+                            });
+                        }
+                    }
+                }
+                return res;
+            },
+        },
+        methods: {
+            add_annotation() {
+                let x1 = new Date(this.results[Math.floor(Math.random() * this.results.length)].test_week).getTime();
+                let x2 = x1;
+                if (Math.random() > 0.5)
+                    x2 = new Date(this.results[Math.floor(Math.random() * this.results.length)].test_week).getTime();
+                if (x1 > x2) {
+                    let t = x1;
+                    x1 = x2;
+                    x2 = t;
+                }
+                this.$refs.chart.addXaxisAnnotation({
+                    x: x1,
+                    x2: x2,
+                    strokeDashArray: 0,
+                    borderColor: '#775DD0',
+                    label: {
+                        text: 'Lorem Ipsum'
+                    },
+                }, true);
+            },
+            auto_scroll(element) { //Scrollt Seite, bis übergebenes Element sichtbar ist.
+                window.$(element)[0].scrollIntoView(false);
+            },
+        },
+        name: "graph-view.vue"
+    }
+</script>
+
+<style scoped>
+</style>
