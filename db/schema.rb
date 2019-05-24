12e4aa3e91d52a4c66cd54ee95572741e3ab8db0
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-05-24 23:55:06 +0200

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_144757) do
+ActiveRecord::Schema.define(version: 2019_03_01_104250) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -130,13 +130,13 @@ ActiveRecord::Schema.define(version: 2019_05_24_144757) do
     t.integer "test_family_id"
     t.string "level"
     t.string "shorthand"
-    t.text "description"
+    t.text "full_description"
+    t.text "short_description"
     t.boolean "student_test"
     t.boolean "archive", default: false
     t.text "configuration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "short_description"
     t.index ["test_family_id"], name: "index_tests_on_test_family_id"
   end
 
