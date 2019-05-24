eb1747464e243096d4d788f04ba06a9517bed3ee
Morten Bastian <stu114712@mail.uni-kiel.de>
2019-05-24 19:00:30 +0200

    merge + Bugfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_104250) do
+ActiveRecord::Schema.define(version: 2019_05_24_144757) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -136,6 +136,7 @@ ActiveRecord::Schema.define(version: 2019_03_01_104250) do
     t.text "configuration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "short_description"
     t.index ["test_family_id"], name: "index_tests_on_test_family_id"
   end
 
