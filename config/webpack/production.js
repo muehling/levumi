0d564000cf7234786cb01033d7786e5cf01489ca
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-07-24 23:52:44 +0200

    webpack optimization

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = merge(environment.toWebpackConfig(), {
-    mode: 'production'
-})
+module.exports = environment.toWebpackConfig()
+module.exports.mode = 'production'
