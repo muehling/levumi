d7f43bbf9c273492459cdfdafe7dfa4292d0fd7b
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-08-30 13:40:45 +0200

    Kommentar Button erstmal raus

--- app/javascript/packs/home/analysis-view.vue
+++ app/javascript/packs/home/analysis-view.vue
@@ -43,7 +43,8 @@
         <b-row :hidden="!graph_visible">
             <b-col>
                 <div class='ml-2'>
-                    <b-button id='comment_btn' size='sm' variant='outline-secondary' v-b-toggle="'annotation_collapse'">
+                    <!-- TEMPCODE Kommentare! -->
+                    <b-button :hidden="true" id='comment_btn' size='sm' variant='outline-secondary' v-b-toggle="'annotation_collapse'">
                         Anmerkungen
                         <i class='when-closed fas fa-caret-down'></i>
                         <i class='when-opened fas fa-caret-up'></i>
