a7548abc57df8e3f4ad81f1c88cac97b6ef90225
Andreas Muehling <andreas.muehling@informatik.uni-kiel.de>
2019-09-09 18:58:05 +0200

    Bugfix ResultsController

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,37 +1,36 @@
 class ResultsController < ApplicationController
   before_action :set_student
 
-  before_action :check_login, only: [:edit, :update]
-  skip_before_action :set_login, only: [:create, :edit, :update]
+  before_action :check_login, only: [:create]
+  skip_before_action :set_login, only: [:create]
 
   #POST /students/:student_id/results
   def create
-    @test = Test.find(params[:test_id])
-    unless @test.nil?
-      a = Assessment.where(group_id: @student.group_id, test_id: @test.id).first       #Assessment aus student_id und test_id bestimmen
-      @result = @student.results.create(assessment: a)
-      if @test.student_test         #Reicht das immer aus?
-        @redirect = '/testen'
-      else
-        if params.has_key? :student
-          @redirect = student_path(@result.student)
+    if params.has_key?(:test_id)           #Eigentlich "new" Action => Kein Objekt anlegen, Testseite rendern
+      @test = Test.find(params[:test_id])
+      unless @test.nil?
+        @assessment = Assessment.where(group_id: @student.group_id, test_id: @test.id).pluck(:id).first       #Assessment aus student_id und test_id bestimmen
+        @last_result = @student.results.where(assessment_id: @assessment).order(:test_week).last   #Letztes Ergebnis aus der Datenbank
+        if @test.student_test         #Reicht das immer aus?
+          @redirect = '/testen'
         else
-          @redirect = group_assessment_path(@result.student.group, @test)
+          if params.has_key? :student
+            @redirect = student_path(@student)
+          else
+            @redirect = group_assessment_path(@student.group, @test)
+          end
         end
+        render 'edit', layout: 'testing'
       end
-      render 'edit', layout: 'testing'
+    else      #Ergebnis eines Tests => Objekt anlegen
+      @result = @student.results.build(assessment_id: params[:assessment_id])
+      @result.views = JSON.parse(params[:views])
+      @result.report = JSON.parse(params[:report])
+      @result.data = JSON.parse(params[:data])
+      @result.test_date = DateTime.now
+      @result.save
+      head :ok
     end
-  end
-
-  #PUT /students/:student_id/results/:id
-  def update
-    @result = Result.find(params[:id])
-    @result.views = JSON.parse(params[:views])
-    @result.report = JSON.parse(params[:report])
-    @result.data = JSON.parse(params[:data])
-    @result.test_date = DateTime.now
-    @result.save
-    head :ok
   end
 
   #DEL /students/:student_id/results/:id
