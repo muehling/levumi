b6fa99a4ca35fe986547ffb5d6b0be658580a310
Jens Beckmann <jbe@informatik.uni-kiel.de>
2022-11-21 13:32:02 +0100

    Re-add QR code generator to application.js

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -15,6 +15,8 @@ import MaterialsApp from '../vue/materials/materials-app.vue'
 import StudentView from '../vue/testing/student-view.vue'
 import UsersApp from '../vue/users/users-app.vue'
 
+import VueQRCodeComponent from 'qrcode.vue'
+
 import { createPinia, PiniaVuePlugin } from 'pinia' // used for global stores
 
 import router from '../vue/routes/frontend-routes'
@@ -22,7 +24,6 @@ import router from '../vue/routes/frontend-routes'
 import '../styles/application.scss'
 
 window.bootstrap = bootstrap
-
 const element = document.getElementById('levumi')
 
 const init = async () => {
@@ -30,6 +31,7 @@ const init = async () => {
   const data = JSON.parse(element.getAttribute('data')) || {}
 
   Vue.use(BootstrapVue)
+  Vue.component('QrCode', VueQRCodeComponent)
 
   Vue.mixin({
     data: function () {
