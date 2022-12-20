a5a7a724ee3d8097a8c7fbc73ca5c4f9accf9ea2
Jens Beckmann <jbe@informatik.uni-kiel.de>
2022-12-20 08:42:52 +0100

    Fix array error in user helpers

--- app/frontend/utils/user.js
+++ app/frontend/utils/user.js
@@ -1,8 +1,13 @@
-export const isAdmin = capabilities => capabilities.includes('admin') || capabilities === 'admin'
+import isArray from 'lodash/isArray'
+
+export const isAdmin = capabilities =>
+  isArray(capabilities) ? capabilities => capabilities.includes('admin') : capabilities === 'admin'
 
 export const isRegularUser = capabilities => !capabilities
 
 export const hasCapability = (capability, capabilities) =>
-  capabilities?.includes(capability) || capabilities?.includes('admin') || capabilities === 'admin'
+  isArray(capabilities)
+    ? capabilities?.includes(capability) || capabilities?.includes('admin')
+    : capabilities === 'admin'
 
 export const isRegistered = user => user.intro_state > 2
