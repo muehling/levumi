20fc33692265b517900b67ab8a0e19498fd1252f
Jens Beckmann <jbe@informatik.uni-kiel.de>
2024-07-23 08:54:29 +0200

    Upgrade to Rails 7.1.3.2

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.1.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem 'rails', '~> 7.0.3', '>= 7.0.4.1'
+gem 'rails', '>= 7.0.8.4'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem 'sprockets-rails'
@@ -32,4 +32,4 @@ end
 
 gem 'mysql2'
 
-gem "bundle-audit", "~> 0.1.0"
+gem 'bundle-audit', '~> 0.1.0'
--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,17 @@ require_relative './initializers/custom_action_mailer_logger'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Levumi2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
