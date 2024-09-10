f21aa3eaff6d50573503d82e0bc1dba7fa4c03a0
Jens Beckmann <jbe@informatik.uni-kiel.de>
2024-09-10 10:09:43 +0200

    Change mail subject

--- config/initializers/mail_subjects.rb
+++ config/initializers/mail_subjects.rb
@@ -2,12 +2,11 @@ module MailSubjects
   NON_LOGGABLE = {
     WELCOME: 'Herzlich Willkommen bei Levumi!',
     NEW_SHARE: 'Levumi: eine Klasse wurde mit Ihnen geteilt!',
-    PASSWORD_RESET: 'Ihr Passwort wurde entschlüsselt, falls Sie dies nicht veranlasst haben ändern Sie umgehend Ihr Passwort'
+    PASSWORD_RESET: 'Levumi - Passwort zurücksetzen'
   }
   LOGGABLE = {
     NEWS: 'Neuigkeiten von Levumi',
     UNSPECIFIC_SUPPORT: 'Nachricht vom Levumi-Kontaktformular',
     SPECIFIC_SUPPORT: 'Nachricht von Levumi - '
   }
-
 end
